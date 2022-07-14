b2d744c5481fdca9a02f4ba124040111b9576a7b
igregorioneto <igregoriodoneto@outlook.com>
2022-07-13 23:45:00 -0300

    Estrutura do projeto do CRUD

---
+++ src/app/shared/appointment.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Appointment } from '../shared/Appointment';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject,
+} from '@angular/fire/compat/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentService {
+  bookingListRef: AngularFireList<any>;
+  bookingRef: AngularFireObject<any>;
+
+  constructor(private db: AngularFireDatabase) { }
+
+  createBooking(apt: Appointment) {
+    return this.bookingListRef.push({
+      name: apt.name,
+      email: apt.email,
+      mobile: apt.mobile,
+    });
+  }
+
+  getBooking(id: string) {
+    this.bookingRef = this.db.object('/appointment/' + id);
+    return this.bookingRef;
+  }
+
+  getBookingList() {
+    this.bookingListRef = this.db.list('/appointment');
+    return this.bookingListRef;
+  }
+
+  updateBooking(id: string, apt: Appointment) {
+    return this.bookingRef.update({
+      name: apt.name,
+      email: apt.email,
+      mobile: apt.mobile,
+    });
+  }
+
+  deleteBooking(id: string) {
+    this.bookingRef = this.db.object('/appointment/' + id);
+    this.bookingRef.remove();
+  }
+
+}
