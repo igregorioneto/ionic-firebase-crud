a11e0f7d3eaa311f3d4fe10d21deafa2978c65aa
igregorioneto <igregoriodoneto@outlook.com>
2022-07-14 21:45:57 -0300

    Estrutura do Delete e Update

--- src/app/edit-appointment/edit-appointment.page.ts
+++ src/app/edit-appointment/edit-appointment.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { AppointmentService } from './../shared/appointment.service';
 
 @Component({
   selector: 'app-edit-appointment',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-appointment.page.scss'],
 })
 export class EditAppointmentPage implements OnInit {
-
-  constructor() { }
+  updateBookingForm: FormGroup;
+  id: any;
+  constructor(
+    private aptService: AppointmentService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+  ) {
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.aptService.getBooking(this.id).valueChanges().subscribe(res => {
+      this.updateBookingForm.setValue(res);
+    });
+  }
 
   ngOnInit() {
+    this.updateBookingForm = this.fb.group({
+      name: [''],
+      email: [''],
+      mobile: ['']
+    });
+    console.log(this.updateBookingForm.value);
+  }
+
+  updateForm() {
+    this.aptService.updateBooking(this.id, this.updateBookingForm.value)
+      .then(() => {
+        this.router.navigate(['/home']);
+      })
+      .catch(error => console.log(error));
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/dot-notation */
+/* eslint-disable prefer-const */
+import { Component, OnInit } from '@angular/core';
+import { AppointmentService } from '../shared/appointment.service';
+import { Appointment } from '../shared/Appointment';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  Bookings = [];
+  constructor(
+    private aptService: AppointmentService,
+  ) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.fetchBookings();
+    let bookingRes = this.aptService.getBookingList();
+    bookingRes.snapshotChanges().subscribe(res => {
+      this.Bookings = [];
+      res.forEach(item => {
+        let a = item.payload.toJSON();
+        a['$key'] = item.key;
+        this.Bookings.push(a as Appointment);
+      });
+    });
+  }
+
+  fetchBookings() {
+    this.aptService.getBookingList().valueChanges().subscribe(res => {
+      console.log(res);
+    });
+  }
+
+  deleteBooking(id: string) {
+    console.log(id);
+    if (window.confirm('Do you really want to delete?')) {
+      this.aptService.deleteBooking(id);
+    }
+  }
 
 }
